da614a6135f8a874941e0146c7015edb4d519fab
AndyArrieta <andrestransformers@hotmail.com>
2023-02-19 23:23:38 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoBiblioteca/Models/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBiblioteca.Models
+{
+    public class Persona
+    {
+        public int IdPersona { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public string Codigo { get; set; }
+        public string HoraI { get; set; }
+        public string HoraS { get; set; }
+        public string Dias { get; set; }
+        public string Especialidad { get; set; }
+        public TipoPersona oTipoPersona { get; set; }
+        public bool Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
---
+++ ProyectoBiblioteca/Models/TipoPersona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBiblioteca.Models
+{
+    public class TipoPersona
+    {
+        public int IdTipoPersona { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
